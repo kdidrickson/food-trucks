a467999f238546c278425b337428650d45c86803
Kristof Didrickson <kristof.didrickson@lionassociates.com>
2017-07-23 13:46:06 -0700

    Change config info to app info

--- src/config.js
+++ src/config.js
@@ -15,21 +15,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'SF Food Trucks',
+    description: 'Find food trucks in San Francisco',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'SF Food Trucks | %s',
       meta: [
-        {name: 'description', content: 'All the modern best practices in one example.'},
+        {name: 'description', content: 'Find food trucks in San Francisco'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'SF Food Trucks'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'SF Food Trucks'},
+        {property: 'og:description', content: 'Find food trucks in San Francisco'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@kdidrickson'},
+        {property: 'og:creator', content: '@kdidrickson'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
