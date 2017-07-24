44582a7a7455afa99877b9d61c616fbdfa5bedcb
Kristof Didrickson <kristof.didrickson@lionassociates.com>
2017-07-24 02:24:16 -0700

    deploy bugs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 require('babel-polyfill');
 
-const devApiKeys = require('../devApiKeys.js') || {};
-
 const environment = {
   development: {
     isProduction: false
@@ -11,6 +9,14 @@ const environment = {
   }
 }[process.env.NODE_ENV || 'development'];
 
+if (environment === 'development') {
+  try {
+    const devApiKeys = require('../devApiKeys.js') || {};
+  } catch (error) {
+
+  }
+}
+
 module.exports = Object.assign({
   host: process.env.HOST || 'localhost',
   port: process.env.PORT,
@@ -19,12 +25,12 @@ module.exports = Object.assign({
   apis: {
     dataSf: {
       endpoint: process.env.DATA_SF_API_ENDPOINT || `https://data.sfgov.org/resource/6a9r-agq8.json`,
-      token: devApiKeys.dataSf && devApiKeys.dataSf.token ? devApiKeys.dataSf.token : process.env.DATA_SF_API_TOKEN,
-      secret: devApiKeys.dataSf && devApiKeys.dataSf.secret ? devApiKeys.dataSf.secret : process.env.DATA_SF_API_SECRET,
+      token: devApiKeys && devApiKeys.dataSf && devApiKeys.dataSf.token ? devApiKeys.dataSf.token : process.env.DATA_SF_API_TOKEN,
+      secret: devApiKeys && devApiKeys.dataSf && devApiKeys.dataSf.secret ? devApiKeys.dataSf.secret : process.env.DATA_SF_API_SECRET,
     },
     googleMaps: {
       endpoint: process.env.GOOGLE_MAPS_API_ENDPOINT || `https://maps.googleapis.com/maps/api/geocode/json`,
-      token: devApiKeys.googleMaps && devApiKeys.googleMaps.token ? devApiKeys.googleMaps.token : process.env.GOOGLE_MAPS_API_TOKEN
+      token: devApiKeys && devApiKeys.googleMaps && devApiKeys.googleMaps.token ? devApiKeys.googleMaps.token : process.env.GOOGLE_MAPS_API_TOKEN
     }
   },
   app: {
